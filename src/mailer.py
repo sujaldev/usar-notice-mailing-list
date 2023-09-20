aef78ffec250e045617ca544074d9f44533b60f2
Sujal Singh <email.sujalsingh@gmail.com>
2023-09-20 22:17:39 +0530

    add mailer

---
+++ src/mailer.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+SENDER = "usar.unofficial@gmail.com"
+PASSWORD = os.environ.get("PASSWORD")
+with open("emails.csv") as file:
+    RECIPIENTS = list(set(file.read().splitlines()))
+
+
+def send_notification(description: str, url: str = None) -> None:
+    body = description if url is None else f"<a href='{url}'>{description}</a>"
+    msg = MIMEText(f"<html><body>{body}</body></html>", "html")
+    msg['Subject'] = description[:72] + ("..." if len(description) > 72 else "")
+    msg['From'] = f"USAR Unofficial <{SENDER}>"
+    msg['To'] = ', '.join(RECIPIENTS)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(SENDER, PASSWORD)
+        smtp.sendmail(SENDER, RECIPIENTS, msg.as_string())
