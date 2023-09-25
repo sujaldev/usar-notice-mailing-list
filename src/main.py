877b178a5d4dbba33c187cb63eccd530f85c3087
Sujal Singh <email.sujalsingh@gmail.com>
2023-09-25 16:50:24 +0530

    drop selenium

--- src/main.py
+++ src/main.py
@@ -1,44 +1,14 @@
 import os
 import logging
 from time import sleep
-from typing import Tuple
 
 from mailer import send_notification
-from scraper import fetch_notifications
-
-LAST_COUNT_FILE = "data/last_count.csv"
-
-
-def check_new_notifications() -> Tuple[bool, dict[str, str]]:
-    """
-    Returns a boolean to indicate whether there are new notifications or not, along with the dictionary containing new
-    notifications if any.
-    """
-    notifications = fetch_notifications()
-
-    # On initialization, we'll assume we've already sent out emails for all existing notifications because assuming
-    # otherwise would just spam the mailing list subscribers.
-    if not os.path.isfile(LAST_COUNT_FILE):
-        with open(LAST_COUNT_FILE, "w") as file:
-            file.write(str(len(notifications)))
-        return False, {}
-
-    with open(LAST_COUNT_FILE, "r+") as file:
-        last_count = int(file.read())
-        new_count = len(notifications)
-
-        assert new_count >= last_count
-        if new_count == last_count:
-            return False, {}
-
-        file.seek(0)
-        file.write(str(new_count))
-        return True, dict(list(notifications.items())[:new_count - last_count])
+from scraper import fetch_new_notifications
 
 
 def main():
-    new_notifications, notifications = check_new_notifications()
-    if new_notifications:
+    has_new, notifications = fetch_new_notifications()
+    if has_new:
         for description, url in notifications.items():
             send_notification(description, url)
 
--- src/scraper.py
+++ src/scraper.py
@@ -4,34 +4,54 @@ extracts the description and the URL (can be None) for all new entries to a dict
 many notifications it saw during the last check and compares that to the number of notifications currently present.
 """
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+import os
+from io import StringIO
+from typing import Tuple
+from urllib.request import urlopen
 
-URL = "https://sites.google.com/view/ggsipuedc/notice-board"
-IFRAME_XPATH = "/html/body/div[1]/div/div[2]/div[3]/div/div[1]/section[6]/div[2]/div/div/div/div/div[2]//iframe"
-ANCHOR_XPATH = "/html/body/table/tbody[2]/tr/td[2]/a"
+from lxml import html
+from lxml.etree import XPath
+
+GENERAL_URL = "https://sites.google.com/view/ggsipuedc/notice-board"
+EXAMINATION_URL = "https://sites.google.com/view/ggsipuedc/examinations/examination-notices"
+LAST_COUNT_FILE = "data/last_count.csv"
 
 
-def fetch_notifications() -> dict[str, str]:
-    driver = webdriver.Remote("http://selenium:4444", options=webdriver.ChromeOptions())
-    driver.get(URL)
+def fetch_notifications(url) -> dict[str, str]:
+    # Returns a dictionary containing notification descriptions as the key, and it's corresponding URL as the value.
+    data_code = html.parse(urlopen(url)).xpath("//@data-code")[0]
+    anchors = XPath("//tbody/tr/td[2]/a")(html.parse(StringIO(data_code)))
+    return {anchor.text.strip(): anchor.get("href") for anchor in anchors}
 
-    try:
-        WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.XPATH, IFRAME_XPATH)))
-    finally:
-        driver.switch_to.frame(driver.find_element(By.XPATH, IFRAME_XPATH))
-        driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe"))
-        driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe"))
-        notifications = {
-            elem.get_attribute("innerText").strip(): elem.get_attribute("href") for elem in
-            driver.find_elements(By.XPATH, ANCHOR_XPATH)
-        }
-        driver.quit()
 
-    return notifications
+def fetch_new_notifications() -> Tuple[bool, dict[str, str]]:
+    gen_notifs, exam_notifs = fetch_notifications(GENERAL_URL), fetch_notifications(EXAMINATION_URL)
+    new_gen_count, new_exam_count = len(gen_notifs), len(exam_notifs)
+
+    # On initialization, we'll assume we've already sent out emails for all existing notifications because assuming
+    # otherwise would just spam the mailing list subscribers.
+    if not os.path.isfile(LAST_COUNT_FILE):
+        with open(LAST_COUNT_FILE, "w") as file:
+            file.write(f"{new_gen_count},{new_exam_count}")
+        return False, {}
+
+    with open(LAST_COUNT_FILE) as file:
+        last_gen_count, last_exam_count = [int(count) for count in file.read().strip().split(",")]
+
+    assert (new_gen_count >= last_gen_count) and (new_exam_count >= last_exam_count)
+
+    with open(LAST_COUNT_FILE, "w") as file:
+        file.write(f"{new_gen_count},{new_exam_count}")
+
+    return (
+        (new_gen_count > last_gen_count) or (new_exam_count > last_exam_count),
+        dict(list(gen_notifs.items())[:new_gen_count - last_gen_count]) |
+        dict(list(exam_notifs.items())[:new_exam_count - last_exam_count])
+    )
 
 
 if __name__ == "__main__":
-    print(fetch_notifications())
+    has_new, notifications = fetch_new_notifications()
+    if has_new:
+        for description, link in notifications.items():
+            print(f"{description} : '{link}'")
